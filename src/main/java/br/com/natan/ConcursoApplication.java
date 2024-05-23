09289332543a38bf729713fadd17d10574718403
Natan Rocha <natanrochabezerra@gmail.com>
2024-05-23 13:34:46 -0300

    first commit

---
+++ src/main/java/br/com/natan/ConcursoApplication.java
@@ -0,0 +1,10 @@
+package br.com.natan;
+
+import org.springframework.boot.SpringApplication;
+
+public class ConcursoApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ConcursoApplication.class, args);
+	}
+}
