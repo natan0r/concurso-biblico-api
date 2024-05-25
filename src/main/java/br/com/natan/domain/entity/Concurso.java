f64ab0793741200ee5289eeb336d89460134eec5
Natan Rocha <natanrochabezerra@gmail.com>
2024-05-25 08:53:37 -0300

    corretion of errors (sql)

--- src/main/java/br/com/natan/domain/entity/Concurso.java
+++ src/main/java/br/com/natan/domain/entity/Concurso.java
@@ -1,5 +1,19 @@
 package br.com.natan.domain.entity;
 
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
 public class Concurso {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private Integer id;
 	
+	@Column(name = "igreja_id")
+	@OneToMany(mappedBy = "igreja" , fetch = FetchType.LAZY)
+	private Integer igreja;
 }
